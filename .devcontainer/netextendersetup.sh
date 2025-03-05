ccb4c028683329d99d358a6eb88514ded03845c6
VINOD APPU <vinod.appu@ashling.com>
2025-03-05 21:46:15 +0530

    removed base image to check

--- .devcontainer/netextendersetup.sh
+++ .devcontainer/netextendersetup.sh
@@ -4,6 +4,6 @@
 # This script is for NetExtender-linux-amd64-10.3.0-21.deb
 mkdir /netextender
 cd /netextender
-wget https://software.sonicwall.com/NetExtender/NetExtender-linux-amd64-10.3.0-21.deb --no-check-certificate
+wget https://software.sonicwall.com/NetExtender/NetExtender-linux-amd64-10.3.0-21.deb
 dpkg -i ./NetExtender-linux-amd64-10.3.0-21.deb
 rm ./NetExtender-linux-amd64-10.3.0-21.deb
