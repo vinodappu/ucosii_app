c4ed749b847d6ba897db5bc3a4ff68d29ffa6589
VINOD APPU <vinod.appu@ashling.com>
2025-03-06 09:11:52 +0530

    Corrected environment variables.

--- .devcontainer/netextendersetup.sh
+++ .devcontainer/netextendersetup.sh
@@ -2,10 +2,9 @@
 # This script is for Ubuntu 22.04.1-Ubuntu SMP Thu Jul 11 22:33:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux
 # This script is for SonicWall NetExtender 10.3.0-21
 # This script is for NetExtender-linux-amd64-10.3.0-21.deb
-mkdir /netextender
 apt-get update
 apt-get install -y wget
-cd /netextender
+cd /tmp
 wget https://software.sonicwall.com/NetExtender/NetExtender-linux-amd64-10.3.0-21.deb
-dpkg -i ./NetExtender-linux-amd64-10.3.0-21.deb
+sudo dpkg -i ./NetExtender-linux-amd64-10.3.0-21.deb
 rm ./NetExtender-linux-amd64-10.3.0-21.deb
